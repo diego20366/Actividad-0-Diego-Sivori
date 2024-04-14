5d9afd8d701dce9675376ec2256ce1c8b77b8024
DiegoSivori <sivori.d@gmail.com>
2024-04-14 19:04:06 -0300

    1.17 genere Http404 si el objeto no existe

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -11,12 +12,9 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-    
+
 def results(request, question_id):
     response = "Estás viendo los resultados de la pregunta %s."
     return HttpResponse(response % question_id)
