35d7e96d6c745ee77a9ad3a94cb2d49bc0989380
DiegoSivori <sivori.d@gmail.com>
2024-04-17 18:39:54 -0300

    1.25 volvi a crear el superusuario porque tuve que eliminar la db.sqlite3, y cree 3 preguntas para la encuesta

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=200, default='default_question_text')
+    pub_date = models.DateTimeField("date published", default=timezone.now)
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
@@ -13,8 +13,9 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=1)  # Suponiendo que el ID de la primera pregunta sea 1
+    choice_text = models.CharField(max_length=200, default='default_value_here')
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
